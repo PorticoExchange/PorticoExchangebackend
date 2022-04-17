6b86ac4baf4e34b5e9361651d2435124c6de09bb
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-04-17 16:53:30 -0300

    Update Consts.ts

--- test/integration/rates/data/Consts.ts
+++ test/integration/rates/data/Consts.ts
@@ -1,7 +1,7 @@
 export const baseAsset = 'LTC';
 export const quoteAsset = 'BTC';
 export const baseAsset = 'USDT';
-export const baseAsset = 'LBTC';
+export const quoteAsset = 'LBTC';
 
 export const checkPrice = (price: number): void => {
   expect(typeof price).toEqual('number');
