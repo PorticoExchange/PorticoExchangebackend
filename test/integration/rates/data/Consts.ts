cd303d2e1e0bbeafa3c1d23048288ff692052182
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-04-17 16:49:13 -0300

    Update Consts.ts
    Update for LBTC and USDT

--- test/integration/rates/data/Consts.ts
+++ test/integration/rates/data/Consts.ts
@@ -1,5 +1,7 @@
 export const baseAsset = 'LTC';
 export const quoteAsset = 'BTC';
+export const baseAsset = 'USDT';
+export const baseAsset = 'LBTC';
 
 export const checkPrice = (price: number): void => {
   expect(typeof price).toEqual('number');
