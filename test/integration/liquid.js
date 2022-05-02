cf5026f48ac99b4eeb09af8ce165a94c4b5f8b4a
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-05-02 13:17:39 -0300

    Update liquid.js

--- test/integration/liquid.js
+++ test/integration/liquid.js
@@ -10,27 +10,27 @@ import { networks as lqnetworks } from 'liquidjs-lib'
 
 describe('liquidclient', () => {
   beforeAll(async () => {
-    await bitcoinLndClient.connect();
+    await liquidLndClient.connect();
   });
 
   afterAll(async () => {
-    bitcoinLndClient.disconnect();
+    liquidLndClient.disconnect();
   });
 
   test('should calculate payment fees', async () => {
-    const calculatePaymentFee = bitcoinLndClient['calculatePaymentFee'];
+    const calculatePaymentFee = liquidLndClient['calculatePaymentFee'];
 
     const bigInvoiceAmount = 8754398;
-    let invoice = await bitcoinLndClient.addInvoice(bigInvoiceAmount);
+    let invoice = await liquidLndClient.addInvoice(bigInvoiceAmount);
 
     // Should use the payment fee ratio for big payments
     expect(calculatePaymentFee(invoice.paymentRequest)).toEqual(Math.ceil(bigInvoiceAmount * LndClient['maxPaymentFeeRatio']));
 
     // Should use the minimal payment fee for small payments
-    invoice = await bitcoinLndClient.addInvoice(1);
+    invoice = await liquidLndClient.addInvoice(1);
     expect(calculatePaymentFee(invoice.paymentRequest)).toEqual(LndClient['minPaymentFee']);
 
-    invoice = await bitcoinLndClient.addInvoice(0);
+    invoice = await liquidLndClient.addInvoice(0);
     expect(calculatePaymentFee(invoice.paymentRequest)).toEqual(LndClient['minPaymentFee']);
   });
 
